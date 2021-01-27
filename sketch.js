6ec32c641098345d29bf3b69c2d226a52f447953
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-01-27 17:09:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var canvas;
-var block1,block2,block3,block4;
+//create 4 blocks
 var ball, edges;
 var music;
 
@@ -11,29 +11,28 @@ function preload(){
 function setup(){
     canvas = createCanvas(800,600);
 
-    block1 = createSprite(0,580,360,30);
+    //
     block1.shapeColor = rgb(0,0,255);
 
-    block2 = createSprite(295,580,200,30);
+    //
     block2.shapeColor = rgb(255,128,0);
 
-    block3 = createSprite(515,580,200,30);
+    //
     block3.shapeColor = rgb(153,0,76);
 
-    block4 = createSprite(740,580,220,30);
+    //
     block4.shapeColor = rgb(0,100,0);
 
-    ball = createSprite(random(20,750),100, 40,40);
+    //create ball with random x pos
     ball.shapeColor = rgb(255,255,255);
-    ball.velocityX = 4;
-    ball.velocityY = 9;
+   //assign velocities
 
 }
 
 function draw() {
     background(rgb(169,169,169));
     edges=createEdgeSprites();
-    ball.bounceOff(edges);
+   //make ball bounce off edges
 
     if(block1.isTouching(ball) && ball.bounceOff(block1)){
         ball.shapeColor = rgb(0,0,255);
